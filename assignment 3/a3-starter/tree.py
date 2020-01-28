8cccbaa62a41c247122d8d4233806c8a2908a58e
andrew <andrew7011616@gmail.com>
2020-01-28 13:54:21 -0800

    add abpruning

--- assignment 3/a3-starter/tree.py
+++ assignment 3/a3-starter/tree.py
@@ -1,82 +1,63 @@
-class node:
-  def __init__(self, state, static_value=None, children=None):
-    self.state = state
-    self.static_value = static_value
-    if children:
-      self.children = children
-    else:
-      self.children = []
-  
-  def add(self,state):
-    new_node = node(state)
-    self.children.append(new_node)
-    return new_node
+def useAlphaBetaPruning(prune=False):
 
-  #assuming state is bgstate type
-  def remove(self,key):
-    for n in self.children:
-      if(
-        n.bar == key.bar[:] and
-        n.white_off == key.white_off[:] and
-        n.red_off == key.red_off[:] and
-        n.cube == key.cube and
-        n.offering_double == key.offering_double and
-        n.whose_move == key.whose_move and
-        n.pointLists == key.pointLists]
-      ):
-        return self.children.pop(n)
-      
+def statesAndCutoffsCounts():
 
-class tree:
-  def __init__(self):
-    self.root = None
+def setMaxPly(maxply=-1):
 
+def useSpecialStaticEval(func):
+
+def staticEval(state):
+
+
+bestMove
 # Minimax Pseudocode
-alpaBetaMinimax(node, alpha, beta) 
+def alpaBetaMinimax(state, alpha, beta, depth, d_limit):
 
    """ 
    Returns best score for the player associated with the given node.
    Also sets the variable bestMove to the move associated with the
    best score at the root node.  
    """
-
+   global bestMove
    # check if at search bound
-   if node is at depthLimit
-      return staticEval(node)
+   if (depth == d_limit):
+      return staticEval(state)
 
    # check if leaf
-   children = successors(node)
-   if len(children) == 0
-      if node is root
+   children = successors(state)
+   if (len(children) == 0):
+      if (depth == 0):
          bestMove = [] 
       return staticEval(node)
 
    # initialize bestMove
-   if node is root
+   if (depth == 0):
       bestMove = operator of first child
       # check if there is only one option
       if len(children) == 1
          return None
 
-   if it is MAX's turn to move
-      for child in children
-         result = alphaBetaMinimax(child, alpha, beta)
-         if result > alpha
+   if (depth%2 == state.whose_move):
+      for (k in children.keys()):
+         result = alphaBetaMinimax(children[k], alpha, beta, depth+1, d_limit)
+         if (result > alpha):
             alpha = result
-            if node is root
-               bestMove = operator of child
-         if alpha >= beta
+            if (depth == 0):
+               bestMove = k
+         if (alpha >= beta):
+            # count++
             return alpha
       return alpha
 
-   if it is MIN's turn to move
-      for child in children
-         result = alphaBetaMinimax(child, alpha, beta)
-         if result < beta
+   else:
+      for (k in children.keys()):
+         result = alphaBetaMinimax(children[k], alpha, beta, depth+1, d_limit)
+         if (result < beta):
             beta = result
-            if node is root
-               bestMove = operator of child
-         if beta <= alpha
+            if (depth == 0):
+               bestMove = k
+         if (beta <= alpha)
+            # count++
             return beta
       return beta
 
